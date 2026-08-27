fc7b300a303c422273f6542d2c13d0583ec22ae0
andreafifcps <andre.afif35@gmail.com>
2026-08-27 23:03:17 +0700

    chore: update legacy CSS and JS files to match modular structure

--- assets/js/m3.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-const drawer=document.getElementById('drawer');
-document.getElementById('open-drawer').addEventListener('click',()=>drawer.open=true);
